397e64f7e221a1fd546377354e3bf010dabe3111
Mukaffi28 <mukaffi28@gmail.com>
2024-02-13 00:02:13 +0600

    Chart added

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .import views
 
+
+
 urlpatterns = [
     path('', views.index, name="index"),
-    path('home/', views.home, name="home")
+    path('home/', views.home, name="home"),
+    path('stock-chart/', views.stock_chart, name='stock_chart'),
+    
 ]
--- crudapp/views.py
+++ crudapp/views.py
@@ -8,6 +8,55 @@ from django.shortcuts import render
 from datetime import datetime
 from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+from django.views import View
+import io
+import matplotlib.pyplot as plt 
+from django.http import HttpResponse
+from django.views import View
+from django.http import JsonResponse
+import plotly.graph_objs as go
+from django.db.models import Max
+
+
+
+def home(request):
+   # load the given json file 
+    with open('E:\Django\crudapplication\crudapp\stock_market_data.json') as f:
+        data = json.load(f)
+        for item in data:
+            item['date'] = datetime.strptime(item['date'], '%Y-%m-%d').date()
+
+            
+            for numeric_field in ['high', 'low', 'open', 'close', 'volume']:
+                value = item[numeric_field].replace(',', '')
+
+               
+                if value == '0':
+                    item[numeric_field] = Decimal('0')
+                else:
+                    try:
+                       
+                        item[numeric_field] = Decimal(value)
+                    except (InvalidOperation, TypeError, ValueError):
+                        
+                        print(f"Error converting {numeric_field} for item: {item}")
+                       
+                        print(f"{numeric_field}: {type(item[numeric_field])}")
+                        item[numeric_field] = None
+
+                
+
+            try:
+                Stock.objects.create(**item)
+            except Exception as e:
+                print(f"Error creating object with item: {item}")
+                print(e)
+
+    # Retrieve all data from the JsonModel
+    data_from_db = Stock.objects.all()
+    
+    return render(request, 'home.html', {'data': data_from_db})
 
 def index(request):
     stocks = Stock.objects.all()
@@ -49,13 +98,13 @@ def index(request):
             stock = Stock.objects.get(id=id)
 
             try:
-                # Convert the input date string to a datetime object
+                
                 date_obj = datetime.strptime(date, '%b. %d, %Y')
             
-                # Format the datetime object as a string in "YYYY-MM-DD" format
+                
                 formatted_date = date_obj.strftime('%Y-%m-%d')
             except ValueError:
-                # Handle the case where the input date string is not in the expected format
+                
                 raise ValidationError('Invalid date format. Please use MMM. D, YYYY format.')
 
             stock.trade_code = trade_code
@@ -75,7 +124,7 @@ def index(request):
         
         elif "search" in request.POST:
             search_query = request.POST.get("query")
-        # Ensure search_query is not None before filtering
+        
         if search_query is not None:
             stocks = Stock.objects.filter(
                 Q(trade_code__icontains=search_query) |
@@ -86,64 +135,170 @@ def index(request):
                 Q(volume__icontains=search_query) |
                 Q(close__icontains=search_query)
         )
-            #stocks = Stock.objects.filter(Q(trade_code__icontains=search_query) | Q(date__icontains=search_query)| Q(low__icontains=search_query) | Q(high__icontains=search_query) | Q(open__icontains=search_query) | Q(volume__icontains=search_query) | Q(close__icontains=search_query))
-    # Set the number of items per page
+            
     per_page = 100
 
-    # Use Django Paginator to paginate the stocks
+    
     paginator = Paginator(stocks, per_page)
     
-    # Get the requested page number from the URL parameter
+    
     page = request.GET.get('page')
 
     try:
-        # Get the stocks for the requested page
+        
         stocks = paginator.page(page)
     except PageNotAnInteger:
-        # If the page parameter is not an integer, show the first page
+       
         stocks = paginator.page(1)
     except EmptyPage:
-        # If the page is out of range, show the last page
+       
         stocks = paginator.page(paginator.num_pages)
     context = {"stocks": stocks,"search_query": search_query}
     return render(request, "index.html", context=context)
 
 
-def home(request):
-    # Load data from JSON file
-    with open('E:\Django\jsonModel\home\stock_market_data.json') as f:
-        data = json.load(f)
-        for item in data:
-            item['date'] = datetime.strptime(item['date'], '%Y-%m-%d').date()
 
-            # Handle numeric field conversions
-            for numeric_field in ['high', 'low', 'open', 'close', 'volume']:
-                value = item[numeric_field].replace(',', '')
 
-                # Check for special cases
-                if value == '0':
-                    item[numeric_field] = Decimal('0')
-                else:
-                    try:
-                        # Try converting to Decimal using Decimal constructor
-                        item[numeric_field] = Decimal(value)
-                    except (InvalidOperation, TypeError, ValueError):
-                        # If it fails, set to None or handle it as needed
-                        print(f"Error converting {numeric_field} for item: {item}")
-                        # Print or log the data types
-                        print(f"{numeric_field}: {type(item[numeric_field])}")
-                        item[numeric_field] = None
 
-                
 
-            try:
-                Stock.objects.create(**item)
-            except Exception as e:
-                print(f"Error creating object with item: {item}")
-                print(e)
+def stock_chart(request):
+    # Get distinct trade codes
+    trade_codes = Stock.objects.values_list('trade_code', flat=True).distinct()
 
-    # Retrieve all data from the JsonModel
-    data_from_db = Stock.objects.all()
+    # Default values
+    selected_trade_code = trade_codes[2]  # Select the first trade code by default
+
+    if 'selected_trade_code' in request.GET:
+        selected_trade_code = request.GET['selected_trade_code']
+
+    # Filter stocks based on selected trade code
+    stocks = Stock.objects.filter(trade_code=selected_trade_code)
+
+    # Extract date, open, close prices, and volume for the line/bar chart
+    dates = [stock.date for stock in stocks]
+    open_prices = [stock.open for stock in stocks]
+    close_prices = [stock.close for stock in stocks]
+    volumes = [stock.volume for stock in stocks]
+
+    # Create the Plotly trace for close prices (line chart)
+    trace_close = go.Scatter(x=dates, y=close_prices, mode='lines', name='Close Prices')
+
+    # Create the Plotly trace for open prices (line chart)
+    trace_open = go.Scatter(x=dates, y=open_prices, mode='lines', name='Open Prices')
+
+    # Create the Plotly trace for volume (bar chart)
+    trace_volume = go.Bar(x=dates, y=volumes, name='Volume', yaxis='y2')
+
+    # Create the Plotly layout for line/bar chart
+    line_bar_chart_layout = go.Layout(
+        title=f'Stock Prices and Volume Over Time for {selected_trade_code}',
+        xaxis=dict(title='Date', range=[min(dates), max(dates)]),
+        yaxis=dict(title='Price', range=[min(min(open_prices), min(close_prices)), max(max(open_prices), max(close_prices))]),
+        yaxis2=dict(title='Volume', overlaying='y', side='right', range=[0, max(volumes)])
+    )
+
+    # Create the Plotly figure with multiple traces and axes for line/bar chart
+    line_bar_chart = go.Figure(data=[trace_close, trace_open, trace_volume], layout=line_bar_chart_layout)
+
+    # Convert the Plotly figure for line/bar chart to JSON
+    line_bar_chart_json = line_bar_chart.to_json()
+
+    # Extract open, high, low, close prices for the candlestick chart
+    open_prices = [stock.open for stock in stocks]
+    high_prices = [stock.high for stock in stocks]
+    low_prices = [stock.low for stock in stocks]
+    close_prices = [stock.close for stock in stocks]
+
+    # Create the Plotly trace for candlestick chart
+    trace_candlestick = go.Candlestick(x=dates,
+                                       open=open_prices,
+                                       high=high_prices,
+                                       low=low_prices,
+                                       close=close_prices,
+                                       name='Candlestick')
+
+    # Create the Plotly layout for candlestick chart
+    candlestick_chart_layout = go.Layout(
+        title=f'Candlestick Chart for {selected_trade_code}',
+        xaxis=dict(title='Date', range=[min(dates), max(dates)]),
+        yaxis=dict(title='Price', range=[min(low_prices), max(high_prices)])
+    )
+
+    # Create the Plotly figure with candlestick trace and layout for candlestick chart
+    candlestick_chart = go.Figure(data=[trace_candlestick], layout=candlestick_chart_layout)
+
+    # Convert the Plotly figure for candlestick chart to JSON
+    candlestick_chart_json = candlestick_chart.to_json()
+
+    # Extract the last volume for each trade code
+    top_10_volumes = Stock.objects.values('trade_code').annotate(last_volume=Max('volume')).order_by('-last_volume')[:10]
+
+    # Create the Plotly trace for pie chart of last volumes for top 10 trade codes
+    trace_pie = go.Pie(labels=[item['trade_code'] for item in top_10_volumes],
+                   values=[item['last_volume'] for item in top_10_volumes],
+                   name='Last Volume')
+
+    # Create the Plotly layout for pie chart
+    pie_chart_layout = go.Layout(title='Top 10 Trade Codes with Highest Trading Volumes',autosize=False,
+    width=500,
+    height=500)
+
+    # Create the Plotly figure with pie chart trace and layout for pie chart
+    pie_chart = go.Figure(data=[trace_pie], layout=pie_chart_layout)
+
+    # Convert the Plotly figure for pie chart to JSON
+    pie_chart_json = pie_chart.to_json()
+
+
+    # Extract the last close price for each trade code
+    top_10_close_prices = Stock.objects.values('trade_code').annotate(last_close_price=Max('close')).order_by('-last_close_price')[:10]
+
+    # Create the Plotly trace for pie chart of last close prices for top 10 trade codes
+    trace_pie_close_prices = go.Pie(labels=[item['trade_code'] for item in top_10_close_prices],
+                                values=[item['last_close_price'] for item in top_10_close_prices],
+                                name='Last Close Price')
+
+    # Create the Plotly layout for pie chart of last close prices
+    pie_chart_layout_close_prices = go.Layout(title='Top 10 Trade Codes by Closing Prices',
+                                          autosize=False,
+                                          width=500,
+                                          height=500)
+
+    # Create the Plotly figure with pie chart trace and layout for pie chart of last close prices
+    pie_chart_close_prices = go.Figure(data=[trace_pie_close_prices], layout=pie_chart_layout_close_prices)
+
+    # Convert the Plotly figure for pie chart of last close prices to JSON
+    pie_chart_json_close_prices = pie_chart_close_prices.to_json()    
+    # Extract the open price for each trade code
+    top_10_open_prices = Stock.objects.values('trade_code').annotate(max_open_price=Max('open')).order_by('-max_open_price')[:10]
     
-    return render(request, 'home.html', {'data': data_from_db})
+    # Create the Plotly trace for pie chart of open prices for top 10 trade codes
+    trace_pie_op = go.Pie(labels=[item['trade_code'] for item in top_10_open_prices],
+                   values=[item['max_open_price'] for item in top_10_open_prices],
+                   name='Open Price')
 
+    # Create the Plotly layout for pie chart
+    pie_chart_layout_op = go.Layout(
+        title='Top 10 Trade Codes by Opening Prices',
+        autosize=False,
+        width=500,
+        height=500
+                    )
+
+    # Create the Plotly figure with pie chart trace and layout for pie chart
+    pie_chart_op = go.Figure(data=[trace_pie_op], layout=pie_chart_layout_op)
+
+    # Convert the Plotly figure for pie chart to JSON
+    pie_chart_json_op = pie_chart_op.to_json()    
+
+
+    context = {
+        'line_bar_chart_json': line_bar_chart_json,
+        'candlestick_chart_json': candlestick_chart_json,
+        'pie_chart_json':pie_chart_json,
+        'pie_chart_json_close_prices':pie_chart_json_close_prices,
+        'pie_chart_json_op':pie_chart_json_op,
+        'trade_codes': trade_codes,
+        'selected_trade_code': selected_trade_code
+    }
+    return render(request, 'stock_chart.html', context)
